9840fef6234b83f22ac0538e45eef46c59d50c01
rthroup7 <45805083+rthroup7@users.noreply.github.com>
2018-12-26 16:24:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -727,4 +727,16 @@ client.on('message', msg => {
 
 });
 
+client.on("message", (message) => {
+    if (message.content.startsWith('-delet')) {
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+
+        let args = message.content.split(' ').slice(1);
+        let channel = message.client.channels.find('name', args.join(' '));
+        if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
+        channel.delete()
+    }
+});  
+
+
 client.login(process.env.BOT_TOKEN);
